63c8c78fabd17b3a59fe18476f50a480a9c79170
Muhammad Apriandito A.S. <m.apriandito@gmail.com>
2023-11-09 08:08:29 +0700

    fix namespace

--- R/fix_ner_result.R
+++ R/fix_ner_result.R
@@ -2,7 +2,7 @@ fix_ner_result <- function(data) {
   data |>
     dplyr::group_by(group = cumsum(grepl("^B-", entity))) |>
     dplyr::summarise(
-      entity = first(entity),
+      entity = dplyr::first(entity),
       score = mean(score),
       word = paste(word, collapse = " "),
       .groups = 'drop'
